51c681bf9ab94ea53135cca8f0468a4570fde3f5
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-01-31 14:38:14 +0700

    Update akjr.py

--- akjr.py
+++ akjr.py
@@ -2,21 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-#Header
-st.header('Nugroho :sparkles:")
+# Header
+st.header('Nugroho :sparkles:')
 st.subheader('Plot')
 
-x = np.linspace(-2. np.pi, 2 np.pi. 1800) Generating x values from -2
-y = np.sin(x) #Calculating sin(x) values
-z = np.cos(x) #Calculating sin(x) values
+# Generating x values from -2 to 2*pi with 1800 points
+x = np.linspace(-2*np.pi, 2*np.pi, 1800)
 
-fig, ax plt.subplots(figsize=(16, 8))
-ax.plot(x, y, label='sin(x)', colors'b') #Plotting sin(x) 
-ax.plot(x, z, label='cos(x)', color='g') #Plotting sin(x) 
-ax.set ylabel("")
+# Calculating sin(x) and cos(x) values
+y = np.sin(x)
+z = np.cos(x)
+
+# Plotting sin(x) and cos(x)
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(x, y, label='sin(x)', color='b')
+ax.plot(x, z, label='cos(x)', color='g')
+
+# Adjusting plot settings
+ax.set_ylabel("")
 ax.set_xlabel("x")
 ax.tick_params(axis='y', labelsize=20)
-ax.set xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
-ax.tick params(axis='x', labelsize=15)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
 
+# Displaying the plot in Streamlit
 st.pyplot(fig)
