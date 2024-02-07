2891642fa130fdf5c85ca5ecf8d0ffdf1d8ef908
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-07 13:40:59 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,24 +1,38 @@
 import streamlit as st
 
 # Header
-st.header('Aksamala :sparkles:')
-
-# Subheader
-st.subheader('Plot')
+st.header('Calculator')
 
 # Membuat dua kolom
 c1, c2 = st.columns(2)
 
 # Kolom pertama
 with c1:
-    x = st.number_input('Suhu', value=100)
+    num1 = st.number_input('Masukkan angka pertama')
 
 # Kolom kedua
 with c2:
-    unit = st.selectbox('Unit', ('C', 'F', 'R', 'K'), key='k1')
+    num2 = st.number_input('Masukkan angka kedua')
 
-# Menampilkan hasil input
-st.write(x, unit)
+# Pilih operasi
+operation = st.selectbox('Pilih operasi', ('+', '-', 'x', '/'))
 
-# Copyright
-st.caption('Aksamala Arfendi')
+# Fungsi untuk melakukan operasi
+def calculate(num1, num2, operation):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == 'x':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:  # Menghindari pembagian dengan nol
+            result = num1 / num2
+        else:
+            result = "Error: Pembagian dengan nol"
+    return result
+
+# Tombol untuk menghitung hasil
+if st.button('Hitung'):
+    result = calculate(num1, num2, operation)
+    st.write('Hasil:', result)
