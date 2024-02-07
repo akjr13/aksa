f47d54ce1db9dc2f31865e4f5baad508947b3b68
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-07 14:07:03 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,27 +1,19 @@
 import streamlit as st
 
-# Header
 st.header('Aksamala :sparkles:')
-
-# Subheader
 st.subheader('Kalkulator')
 
-# Membuat tiga kolom
 c1, c2, c3 = st.columns(3)
 
-# Kolom pertama
 with c1:
     num1 = st.number_input('Masukkan angka pertama', step=1)
 
-# Kolom kedua
 with c2:
     # Pilih operasi
     operation = st.selectbox('Pilih operasi', ('+', '-', 'x', '/'))
 
-# Kolom ketiga
 with c3:
     num2 = st.number_input('Masukkan angka kedua', step=1)
-
 
 def calculate(num1, num2, operation):
     if operation == '+':
@@ -34,7 +26,7 @@ def calculate(num1, num2, operation):
         if num2 != 0:  # Menghindari pembagian dengan nol
             result = num1 / num2
         else:
-            result = "Error: Pembagian dengan nol"
+            result = "Error: 
     return result
 
 result = calculate(num1, num2, operation)
