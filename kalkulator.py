0ea543c954f76dd13bf96151ff08b492207a4759
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-07 14:23:21 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -6,30 +6,30 @@ st.subheader('Kalkulator')
 c1, c2, c3 = st.columns(3)
 
 with c1:
-    num1 = st.number_input('Masukkan angka pertama', step=1)
+    x = st.number_input('Masukkan angka pertama', step=1)
 
 with c2:
     # Pilih operasi
     operation = st.selectbox('Pilih operasi', ('+', '-', 'x', '/'))
 
 with c3:
-    num2 = st.number_input('Masukkan angka kedua', step=1)
+    y = st.number_input('Masukkan angka kedua', step=1)
 
-def calculate(num1, num2, operation):
+def calculate(x , y, operation):
     if operation == '+':
-        result = num1 + num2
+        result = x + y
     elif operation == '-':
-        result = num1 - num2
+        result = x - y
     elif operation == 'x':
-        result = num1 * num2
+        result = x * y
     elif operation == '/':
         if num2 != 0:  # Menghindari pembagian dengan nol
-            result = num1 / num2
+            result = x / y
         else:
             result = "Error: Pembagian dengan nol"
     return result
 
-result = calculate(num1, num2, operation)
+result = calculate( x , y, operation)
 st.write('Hasil:', result)
 
 st.caption('Aksamala Arfendi')
