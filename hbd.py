01b10b525d82cd09b3ff385b3826594334f3d83b
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-09 19:38:29 +0700

    Update hbd.py

--- hbd.py
+++ hbd.py
@@ -1,23 +1,26 @@
 import streamlit as st
 from datetime import datetime
 
-def generate_anniversary_message(years):
+def generate_anniversary_message(pacar_name, years):
     # Daftar ucapan selamat tahun pacar
     anniversary_messages = [
-        "Selamat {} tahun bersama! Semoga cinta kita semakin kuat dan abadi.",
-        "Wah, sudah {} tahun ya kita bersama! Setiap momen bersamamu sangat berharga.",
-        "Hari ini adalah hari spesial, {} tahun bersama! Terima kasih telah menjadi bagian dari hidupku.",
-        "Hari ini adalah perayaan {} tahun cinta kita. Aku sangat bersyukur memiliki kamu di sampingku.",
-        "Tak terasa sudah {} tahun kita berdua. Semoga cinta kita selalu menyala dan tak pernah pudar.",
-        "Happy anniversary yang ke-{}! Semoga tahun ini penuh dengan kebahagiaan dan keberkahan."
+        f"Selamat {years} tahun bersama, {pacar_name}! Semoga cinta kita semakin kuat dan abadi.",
+        f"Wah, sudah {years} tahun ya kita bersama, {pacar_name}! Setiap momen bersamamu sangat berharga.",
+        f"Hari ini adalah hari spesial, {years} tahun bersama, {pacar_name}! Terima kasih telah menjadi bagian dari hidupku.",
+        f"Hari ini adalah perayaan {years} tahun cinta kita, {pacar_name}. Aku sangat bersyukur memiliki kamu di sampingku.",
+        f"Tak terasa sudah {years} tahun kita berdua, {pacar_name}. Semoga cinta kita selalu menyala dan tak pernah pudar.",
+        f"Happy anniversary yang ke-{years}, {pacar_name}! Semoga tahun ini penuh dengan kebahagiaan dan keberkahan."
     ]
     # Memilih ucapan sesuai dengan tahun pacar
-    message = anniversary_messages[min(len(anniversary_messages)-1, years-1)].format(years)
+    message = anniversary_messages[min(len(anniversary_messages)-1, years-1)]
     return message
 
 # Menampilkan header di tengah
 st.markdown("<h1 style='text-align: center;'>Ucapan Tahun Pacar 💖</h1>", unsafe_allow_html=True)
-st.subheader('Masukkan tanggal jadian:')
+st.subheader('Masukkan informasi pacar dan tanggal jadian:')
+
+# Input nama pacar
+pacar_name = st.text_input('Nama Pacar:')
 date = st.date_input('Tanggal Jadian')
 
 # Menghitung tahun dari tanggal jadian hingga sekarang
@@ -30,5 +33,8 @@ if date:
 
     # Tombol untuk menghasilkan ucapan
     if st.button('Generate Ucapan'):
-        anniversary_message = generate_anniversary_message(years)
+        if pacar_name:
+            anniversary_message = generate_anniversary_message(pacar_name, years)
+        else:
+            anniversary_message = generate_anniversary_message("Pacar", years)
         st.write(anniversary_message)
