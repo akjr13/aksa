71b1c0d5d8310f6839b508bcc0dbf9f7c6538240
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-11 09:29:32 +0700

    Update hbd.py

--- hbd.py
+++ hbd.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from datetime import date
 
 # Membuat fungsi untuk menghasilkan ucapan ulang tahun untuk pacar
 def generate_birthday_message(name, birthdate, wish):
@@ -11,11 +10,8 @@ st.markdown("<h1 style='text-align: center;'>Ucapan Ulang Tahun Firnanda Valen ð
 # Input nama pacar
 name = st.text_input('Nama Pacar:')
 
-# Batas atas tanggal lahir hingga tahun 2000
-max_date = date(2000, 1, 1)
-
 # Input tanggal lahir Anda
-birthdate = st.date_input('Tanggal Lahir Anda:', max_value=max_date)
+birthdate = st.date_input('Tanggal Lahir Anda:')
 
 # Input keinginan dari Anda
 wish = st.text_area('Keinginan dari Anda:')
