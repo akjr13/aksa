918b24526c305f4232b0e07c7880896055c30289
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-11 09:26:54 +0700

    Update hbd.py

--- hbd.py
+++ hbd.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # Membuat fungsi untuk menghasilkan ucapan ulang tahun untuk pacar
-def generate_birthday_message(name, age, wish, future_wish):
-    return f"Selamat ulang tahun yang ke-{age}, {name}! Semoga hari ulang tahunmu penuh dengan kebahagiaan dan keceriaan. {wish} {future_wish}"
+def generate_birthday_message(name, birthdate, wish):
+    return f"Selamat ulang tahun, {name}! Semoga hari ulang tahunmu penuh dengan kebahagiaan dan keceriaan. {wish}"
 
 # Menampilkan header di tengah
 st.markdown("<h1 style='text-align: center;'>Ucapan Ulang Tahun Firnanda Valen ðŸ’–</h1>", unsafe_allow_html=True)
@@ -10,17 +10,16 @@ st.markdown("<h1 style='text-align: center;'>Ucapan Ulang Tahun Firnanda Valen ð
 # Input nama pacar
 name = st.text_input('Nama Pacar:')
 
-# Input usia pacar
-age = st.number_input('Usia Pacar:', min_value=1, max_value=150, value=1)
+# Input tanggal lahir Anda
+birthdate = st.date_input('Tanggal Lahir Anda:')
 
 # Input keinginan dari Anda
 wish = st.text_area('Keinginan dari Anda:')
 
-
 # Tombol untuk menampilkan ucapan
 if st.button('Tampilkan Ucapan'):
     if name:
-        birthday_message = generate_birthday_message(name, age, wish, future_wish)
+        birthday_message = generate_birthday_message(name, birthdate, wish)
         st.write(birthday_message)
     else:
         st.error("Mohon masukkan nama pacar untuk menampilkan ucapan ulang tahun.")
