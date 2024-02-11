b2165694c177a95eb161b159ec281dec85e23f59
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-11 09:23:15 +0700

    Update hbd.py

--- hbd.py
+++ hbd.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # Membuat fungsi untuk menghasilkan ucapan ulang tahun untuk pacar
-def generate_birthday_message(name, age, wish):
-    return f"Selamat ulang tahun yang ke-{age}, {name}! Semoga hari ulang tahunmu penuh dengan kebahagiaan dan keceriaan. {wish}"
+def generate_birthday_message(name, age, wish, future_wish):
+    return f"Selamat ulang tahun yang ke-{age}, {name}! Semoga hari ulang tahunmu penuh dengan kebahagiaan dan keceriaan. {wish} {future_wish}"
 
 # Menampilkan header di tengah
 st.markdown("<h1 style='text-align: center;'>Ucapan Ulang Tahun Firnanda Valen 💖</h1>", unsafe_allow_html=True)
@@ -14,12 +14,15 @@ name = st.text_input('Nama Pacar:')
 age = st.number_input('Usia Pacar:', min_value=1, max_value=150, value=1)
 
 # Input keinginan dari Anda
-wish = st.text_area('Keinginan dari Anda:',)
+wish = st.text_area('Keinginan dari Anda:')
+
+# Input harapan Anda untuk masa depan bersama
+future_wish = st.text_area('Harapan Anda untuk Masa Depan:')
 
 # Tombol untuk menampilkan ucapan
 if st.button('Tampilkan Ucapan'):
     if name:
-        birthday_message = generate_birthday_message(name, age, wish)
+        birthday_message = generate_birthday_message(name, age, wish, future_wish)
         st.write(birthday_message)
     else:
         st.error("Mohon masukkan nama pacar untuk menampilkan ucapan ulang tahun.")
