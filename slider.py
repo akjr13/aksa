bd94917c95226a8c8545d8123d727ccf22057a61
akjr13 <158257487+akjr13@users.noreply.github.com>
2024-02-21 14:43:12 +0700

    Update slider.py

--- slider.py
+++ slider.py
@@ -52,7 +52,7 @@ plt.legend()  # Show legend for fig2
 st.pyplot(fig1)
 st.pyplot(fig2)
 
-st.write(f'Integral (using trapezoidal rule) of sin(t) within selected range: {integral_u}')
-st.write(f'Integral (using trapezoidal rule) of f(t) within selected range: {integral_v}')
-st.write(f'Integral (using trapezoidal rule) of sin(t) within selected range {integration_range}: {integral_u}')
-st.write(f'Integral (using trapezoidal rule) of f(t) within selected range {integration_range}: {integral_v}')
+st.write(f'Integral total dari sin(t): {integral_u}')
+st.write(f'Integral total dari f(t): {integral_v}')
+st.write(f'Integral total Rentan sin(t) {integration_range}: {integral_u}')
+st.write(f'Integral total Rentan sin(t) {integration_range}: {integral_v}')
